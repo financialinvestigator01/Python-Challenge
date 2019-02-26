b06593116dfc4e2b783afcb5b87f457be0add16a
Asher Brown <financialinvestigator01@gmail.com>
2019-02-26 10:05:49 -0500

    Completed code to calculate average of the changes in P/L over the entire period

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,16 +12,23 @@ with open(csvpath, newline='') as csvfile:
     # initialize variables and perform calculations
     counter = 0
     net_profit_loss = 0
+    firstrow = True
+    
     for row in csvreader:
-        counter = counter + 1 # Calculate the total number of months in the dataset
+        counter += 1 # Calculate the total number of months in the dataset
         net_profit_loss = float(row[1]) + (float(net_profit_loss)) # Calculate the net total amount of "Profit/Losses" over the entire period
-        
-     
+        if firstrow:
+            firstpl = float(row[1]) #initialize the first value in the list
+            firstrow = False
+        lastpl = float(row[1]) #initialize the last value in the list
+    
+    average_change_PL = float( (lastpl - firstpl) / (counter -1) ) # Calculate the average of the changes in "Profit/Losses" over the entire period
+    average_change_PL = round(average_change_PL, 2)
     print("Total Months: ", counter)
     print("Net Profit/Loss: $", net_profit_loss)
+    print ("Average of changes in Profit/Losses: $", average_change_PL)
 
 
-# Calculate the average of the changes in "Profit/Losses" over the entire period
 # Calculate the greatest increase in profits (date and amount) over the entire period
 # Calculate the greatest decrease in losses (date and amount) over the entire period
 # your analysis should look similar to the one below:

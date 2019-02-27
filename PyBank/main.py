9cd55a3bb16211e201682efffbff2ff3be809a6b
Asher Brown <financialinvestigator01@gmail.com>
2019-02-27 00:07:37 -0500

    Added code to calculate greatest increase and greatest loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,20 +13,44 @@ with open(csvpath, newline='') as csvfile:
     counter = 0
     net_profit_loss = 0
     firstrow = True
-    
+    GreatestProfitIncrease = 0
+    GreatestLossDecrease = 0
+    holder = 0
+
+
     for row in csvreader:
         counter += 1 # Calculate the total number of months in the dataset
-        net_profit_loss = float(row[1]) + (float(net_profit_loss)) # Calculate the net total amount of "Profit/Losses" over the entire period
+        net_profit_loss += float(row[1]) # Calculate the net total amount of "Profit/Losses" over the entire period
         if firstrow:
             firstpl = float(row[1]) #initialize the first value in the list
             firstrow = False
         lastpl = float(row[1]) #initialize the last value in the list
-    
+        
+        month = row[0] # month placeholder
+        amount = row[1] # amount placeholder
+        rowamount = float(amount)
+        difference = rowamount - holder
+        if GreatestProfitIncrease < difference:
+            GreatestProfitIncrease = difference
+            GreatestProfitIncrease_month = month
+        
+        if GreatestLossDecrease > difference:
+            GreatestLossDecrease = difference
+            GreatestLossDecrease_month = month
+        
+        holder = rowamount
+  
     average_change_PL = float( (lastpl - firstpl) / (counter -1) ) # Calculate the average of the changes in "Profit/Losses" over the entire period
     average_change_PL = round(average_change_PL, 2)
+
+    print("Financial Analysis")
+    print("----------------------------------")
     print("Total Months: ", counter)
     print("Net Profit/Loss: $", net_profit_loss)
-    print ("Average of changes in Profit/Losses: $", average_change_PL)
+    print("Average of changes in Profit/Losses: $", average_change_PL)
+    print(f"Greatest Increase in Profits: {GreatestProfitIncrease_month}: (${GreatestProfitIncrease})")
+    print(f"Greatest Decrease in Profits: {GreatestLossDecrease_month}: (${GreatestLossDecrease})")
+
 
 
 # Calculate the greatest increase in profits (date and amount) over the entire period

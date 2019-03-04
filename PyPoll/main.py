890a951c4fbaae842aca51aea6f415523e76e5a0
Asher Brown <financialinvestigator01@gmail.com>
2019-03-03 23:08:28 -0500

    correct printing order of Candidate: percentage of votes (number of votes)

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,39 +14,34 @@ with open(csvpath, newline='') as csvfile:
     # file contains Voter ID, County, Candidate columns
     # initialize variables and initial conditions
        
-    numberholder = 0
+    votes = 0
     candidate_dict = {} # creates an empty dictionary, will be used
                         # to hold the candidate names as keys, and number of votes
                         # as values
 
     for row in csvreader:
-        numberholder += 1 # The total number of votes cast
+        votes += 1 # The total number of votes cast
+        # A complete list of candidates who received votes
         if (row[2] in candidate_dict):      # checks to see if a candidate has been
             candidate_dict[row[2]] += 1     # voted for yet. If yes, keeps a running
         else:                               # total.
             candidate_dict[row[2]] = 1      # if not, adds the new candidate to the 
                                             # dictionary with 1 vote.
 
-             
-# new for loop to determine winner
-     
-
-
-print("values: ", candidate_dict)
-
-#    A complete list of candidates who received votes
-
-#    The percentage of votes each candidate won
-
-#    The total number of votes each candidate won
-
-#    The winner of the election based on popular vote.
-
-
 print("Election Results")
 print("------------------------------")
-print("Total number of votes: ", numberholder)
+print("Total number of votes: ", votes)
 print("------------------------------")
+
+winner = 0
+
+for key, value in candidate_dict.items():
+    percentage_votes = value / votes                            # The percentage of votes each candidate won
+    percentage_votes = "{:.3%}".format(percentage_votes)        # Percentage formatting
+    print("%s : %s  (""%d"")"%(key,percentage_votes, value))    # correct printing format
+    
+  
+#    The winner of the election based on popular vote.
 
 
 # As an example, your analysis should look similar to the one below:
